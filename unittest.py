f67b105ca7cbc41cdb02511445efefb0b514bbf1
RukeshAthikari28 <rukeshathikari@gmail.com>
2025-01-06 11:46:14 +0530

    unittest.py

--- unittest.py
+++ unittest.py
@@ -11,7 +11,7 @@ class TestStringMethods(unittest.TestCase):
     def test_split(self):
         string_input = 'optimal XAI'
         self.assertEqual(string_input.split(), ['optimal', 'XAI'])
-        # check that string_input.split fails when the separator is not a string
+
         with self.assertRaises(TypeError):
             string_input.split(2)
 if __name__ == '__main__':
